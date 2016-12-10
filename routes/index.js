14098402b9298a0852cd1a531a0e4a44d9cc806b
Sneha Sheth <sneha.sheth@addteq.com>
2016-12-10 00:03:16 -0500

    some changes

--- routes/index.js
+++ routes/index.js
@@ -30,11 +30,6 @@ router.get("/about-us", function(req, res, next)
   res.render("about-us");
 });
 
-router.get("/categories", function(req, res, next) 
-{
-  res.render("categories");
-});
-
 router.get("/upload",function(req,res){
   res.render("user/upload");
 });
